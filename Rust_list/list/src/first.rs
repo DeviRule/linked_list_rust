0f7897bceefef12a99bebfd0d8116e3b1ed5087f
wanghulin <hulwang@ucdavis.edu>
2018-02-05 17:31:01 -0800

    final code

--- Rust_list/list/src/first.rs
+++ Rust_list/list/src/first.rs
@@ -28,12 +28,36 @@ impl List{
             Link::Empty=>{
                    None
             } 
-            Link::More(node) =>{
+            Link::More(boxed_node) =>{
+                let node = *boxed_node;
                 self.head = node.next;
                 Some(node.elem)
             }
 
     }
         
+    }
+}
+
+
+mod test{
+    use super::*;
+    #[test]
+    fn basics(){
+        let mut list = List::new();
+        assert_eq!(list.pop(),None);
+
+        //Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        assert_eq!(list.pop(),Some(3));
+        assert_eq!(list.pop(),Some(2));
+        list.push(4);
+        list.push(5);
+        assert_eq!(list.pop(),Some(5));
+        assert_eq!(list.pop(),Some(4));
+        assert_eq!(list.pop(),Some(1));
+        assert_eq!(list.pop(),None);
     }
 }
